69aa861862684904c08354602a3787bda06ee7ab
danielski666 <48628299+danielski666@users.noreply.github.com>
2022-04-13 00:43:38 +0200

    Add files via upload
    First project commit.

---
+++ Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="home">
+	<navbar />
+	<carousel />
+	<products />
+	</div>
+</template>
+
+<script>
+import navbar from '/Navbar.vue'
+import carousel from '/Carousel.vue'
+import products from '/Products.vue'
+export default {
+	name: 'home',
+	components: {
+		navbar,
+		carousel,
+		products
+	}
+}
+</script>
