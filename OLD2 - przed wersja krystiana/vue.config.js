9840a77dca96bb00b8e2cfe4902bf69699b3a2a5
d i <danielski666>
2022-04-26 23:58:00 +0200

    adding dist subtree

--- OLD2 - przed wersja krystiana/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  transpileDependencies: true,
-  lintOnSave:false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/project-99/'
-    : '/'
-}
--- src/components/firebaseInit.js
+++
@@ -1,5 +0,0 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import firebaseConfig from './firebaseConfig';
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-export default firebaseApp.firestore();
