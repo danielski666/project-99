5739b11bcd4e59c92f056eeed5bd7927e66d7a1d
d i <danielski666>
2022-04-29 12:03:22 +0200

    back - 28043 + naglowki

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 		<b-navbar toggleable="lg" type="dark" variant="success">
 			<b-navbar-brand @click="goHome" href="#">
 				<!-- <b-icon-brightness-fill-high></b-icon-brightness-fill-high> -->
-				Ecommerce</b-navbar-brand>
+				READ:ME</b-navbar-brand>
 			
 			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 			
